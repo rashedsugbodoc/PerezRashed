b3bf7389596fbaa06eab9df4836e3436a75ef986
Ryan Michael Gelig <ryan.gelig@gmail.com>
2021-10-16 15:52:34 +0800

    Fixed department sidebar link to be clickable

--- application/modules/home/views/dashboardv3.php
+++ application/modules/home/views/dashboardv3.php
@@ -208,7 +208,7 @@ if (!$this->ion_auth->in_group(array('superadmin'))) {
                         <?php if ($this->ion_auth->in_group('admin')) { ?>
                             <?php if (in_array('department', $this->modules)) { ?>
                                 <li class="slide">
-                                    <a class="side-menu__item"  data-toggle="slide" href="department">
+                                    <a class="side-menu__item" href="department">
                                     <svg class="side-menu__icon" xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><rect fill="none" height="24" width="24"/><g fill="currentColor"><path d="M22,11V3h-7v3H9V3H2v8h7V8h2v10h4v3h7v-8h-7v3h-2V8h2v3H22z M7,9H4V5h3V9z M17,15h3v4h-3V15z M17,5h3v4h-3V5z"/><polyline opacity=".3" points="7,5 7,9 4,9 4,5 7,5"/><polyline opacity=".3" points="20,5 20,9 17,9 17,5 20,5"/><polyline opacity=".3" points="20,15 20,19 17,19 17,15 20,15"/></g></svg>
                                     <span class="side-menu__label"><?php echo lang('departments'); ?></span></a>
                                 </li>
