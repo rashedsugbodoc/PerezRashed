57e6a6f45e28212c1e69307c10b1678ed3583f47
Ryan Michael Gelig <ryan.gelig@gmail.com>
2021-10-18 02:00:44 +0800

    Fixed panel ui view for User Activity Report and date search

--- application/modules/finance/views/user_activity_report.php
+++ application/modules/finance/views/user_activity_report.php
@@ -87,7 +87,7 @@
                                     }
                                 }
                                 ?>">
-                                    <i class="fa fa-search"></i> Today
+                                    <i class="fa fa-search"></i><?php echo lang('today');?>
                                 </button>
                             </div>
                         </a>
@@ -186,154 +186,145 @@
 
                 <?php } ?>
 
-
-
-
                 <?php if ($this->ion_auth->in_group(array('admin')) || $this->ion_auth->get_user_id() == '341') { ?>
-
-                    <section class="col-md-6 no-print">
-                        <form role="form" class="f_report" action="finance/allUserActivityReportDateWise" method="post" enctype="multipart/form-data">
-                            <div class="form-group">
-                                <!--     <label class="control-label col-md-3">Date Range</label> -->
-                                <div class="col-md-6">
-                                    <div class="input-group input-large" data-date="13/07/2013" data-date-format="mm/dd/yyyy">
-                                        <input type="text" class="form-control dpd1" name="date_from" value="<?php
-                                        if (!empty($date_from)) {
-                                            echo date('m/d/Y', $date_from);
-                                        }
-                                        ?>" placeholder="<?php echo lang('date_from'); ?>" readonly="">
-                                        <span class="input-group-addon"><?php echo lang('to'); ?></span>
-                                        <input type="text" class="form-control dpd2" name="date_to" value="<?php
-                                        if (!empty($date_to)) {
-                                            echo date('m/d/Y', $date_to);
-                                        }
-                                        ?>" placeholder="<?php echo lang('date_to'); ?>" readonly="">
-                                        <input type="hidden" class="form-control dpd2" name="user" value="<?php echo $user->ion_user_id; ?>">
+                    <div class="row">
+                        <section class="col-md-12 no-print">
+                            <form role="form" class="f_report" action="finance/allUserActivityReportDateWise" method="post" enctype="multipart/form-data">
+                                <div class="form-group">
+                                    <!--     <label class="control-label col-md-3">Date Range</label> -->
+                                    <div class="col-md-8">
+                                        <div class="input-group input-large" data-date="13/07/2013" data-date-format="mm/dd/yyyy">
+                                            <input type="text" class="form-control dpd1" name="date_from" value="<?php
+                                            if (!empty($date_from)) {
+                                                echo date('m/d/Y', $date_from);
+                                            }
+                                            ?>" placeholder="<?php echo lang('date_from'); ?>" readonly="">
+                                            <span class="input-group-addon"><?php echo lang('to'); ?></span>
+                                            <input type="text" class="form-control dpd2" name="date_to" value="<?php
+                                            if (!empty($date_to)) {
+                                                echo date('m/d/Y', $date_to);
+                                            }
+                                            ?>" placeholder="<?php echo lang('date_to'); ?>" readonly="">
+                                            <input type="hidden" class="form-control dpd2" name="user" value="<?php echo $user->ion_user_id; ?>">
+                                        </div>
+                                        <div class="row"></div>
+                                        <span class="help-block"></span> 
                                     </div>
-                                    <div class="row"></div>
-                                    <span class="help-block"></span> 
+                                    <div class="col-md-4 no-print text-center">
+                                        <button type="submit" name="submit" class="btn btn-primary range_submit"><?php echo lang('submit'); ?></button>
+                                    </div>
                                 </div>
-                                <div class="col-md-6 no-print">
-                                    <button type="submit" name="submit" class="btn btn-primary range_submit"><?php echo lang('submit'); ?></button>
-                                </div>
-                            </div>
-                        </form>
-                    </section>
-
+                            </form>
+                        </section>
+                    </div>
                 <?php } ?>
-
-
-
             </div>
             <div class="col-md-8 row">
-                <div class="adv-table editable-table ">
-                    <header class="panel-heading">
-                        <?php echo lang('all_bills'); ?>
-                    </header>
-                    <div class="space15"></div>
-                    <table class="table table-striped table-hover table-bordered" id="editable-samples">
-                        <thead>
-                            <tr>
-                                <th class=""><?php echo lang('date'); ?></th>
-                                <th class=""><?php echo lang('invoice'); ?> #</th>
-                                <th class=""><?php echo lang('bill_amount'); ?></th>
-                                <th class=""><?php echo lang('deposit'); ?></th>
-                            </tr>
-                        </thead>
-                        <tbody>
+                <div class="panel">
+                    <div class="adv-table editable-table ">
+                        <header class="panel-heading">
+                            <?php echo lang('all_bills'); ?>
+                        </header>
+                        <div class="space15"></div>
+                        <table class="table table-striped table-hover table-bordered" id="editable-samples">
+                            <thead>
+                                <tr>
+                                    <th class=""><?php echo lang('date'); ?></th>
+                                    <th class=""><?php echo lang('invoice'); ?> #</th>
+                                    <th class=""><?php echo lang('bill_amount'); ?></th>
+                                    <th class=""><?php echo lang('deposit'); ?></th>
+                                </tr>
+                            </thead>
+                            <tbody>
 
-                            <?php
-                            $dates = array();
-                            $datess = array();
-                            foreach ($payments as $payment) {
-                                $dates[] = $payment->date;
-                            }
-                            foreach ($deposits as $deposit) {
-                                $datess[] = $deposit->date;
-                            }
-                            $dat = array_merge($dates, $datess);
-                            $dattt = array_unique($dat);
-                            asort($dattt);
-
-                            $total_payment = array();
-
-                            $total_deposit = array();
-                            ?>
-
-                            <?php
-                            foreach ($dattt as $key => $value) {
+                                <?php
+                                $dates = array();
+                                $datess = array();
                                 foreach ($payments as $payment) {
-                                    if ($payment->date == $value) {
-                                        $total_payment[] = $payment->gross_total;
-                                        ?>
-                                        <tr class="">
-                                            <td><?php echo date('d/m/y', $payment->date); ?></td>
-                                            <td> <?php echo $payment->id; ?></td>
-                                            <td><?php echo $settings->currency; ?> <?php echo $payment->gross_total; ?></td>
-                                            <td><?php
-                                                if (!empty($payment->amount_received)) {
-                                                    echo $settings->currency;
-                                                }
-                                                ?> <?php echo $payment->amount_received; ?></td>
-
-                                        </tr>
-
-                                        <?php
-                                    }
+                                    $dates[] = $payment->date;
                                 }
+                                foreach ($deposits as $deposit) {
+                                    $datess[] = $deposit->date;
+                                }
+                                $dat = array_merge($dates, $datess);
+                                $dattt = array_unique($dat);
+                                asort($dattt);
+
+                                $total_payment = array();
+
+                                $total_deposit = array();
                                 ?>
 
                                 <?php
-                                foreach ($deposits as $deposit) {
-                                    if ($deposit->date == $value) {
-                                        $total_deposit[] = $deposit->deposited_amount;
-                                        if (!empty($deposit->deposited_amount) && empty($deposit->amount_received_id)) {
+                                foreach ($dattt as $key => $value) {
+                                    foreach ($payments as $payment) {
+                                        if ($payment->date == $value) {
+                                            $total_payment[] = $payment->gross_total;
                                             ?>
-
                                             <tr class="">
-                                                <td><?php echo date('d-m-y', $deposit->date); ?></td>
-                                                <td><?php echo $deposit->payment_id; ?></td>
-                                                <td></td>
-                                                <td><?php echo $settings->currency; ?> <?php echo $deposit->deposited_amount; ?></td>
+                                                <td><?php echo date('d/m/y', $payment->date); ?></td>
+                                                <td> <?php echo $payment->id; ?></td>
+                                                <td><?php echo $settings->currency; ?> <?php echo $payment->gross_total; ?></td>
+                                                <td><?php
+                                                    if (!empty($payment->amount_received)) {
+                                                        echo $settings->currency;
+                                                    }
+                                                    ?> <?php echo $payment->amount_received; ?></td>
 
                                             </tr>
+
                                             <?php
                                         }
                                     }
+                                    ?>
+
+                                    <?php
+                                    foreach ($deposits as $deposit) {
+                                        if ($deposit->date == $value) {
+                                            $total_deposit[] = $deposit->deposited_amount;
+                                            if (!empty($deposit->deposited_amount) && empty($deposit->amount_received_id)) {
+                                                ?>
+
+                                                <tr class="">
+                                                    <td><?php echo date('d-m-y', $deposit->date); ?></td>
+                                                    <td><?php echo $deposit->payment_id; ?></td>
+                                                    <td></td>
+                                                    <td><?php echo $settings->currency; ?> <?php echo $deposit->deposited_amount; ?></td>
+
+                                                </tr>
+                                                <?php
+                                            }
+                                        }
+                                    }
+                                    ?>
+                                <?php } ?>
+
+                                <?php
+                                if (!empty($total_payment)) {
+                                    $total_p = array_sum($total_payment);
+                                } else {
+                                    $total_p = 0;
+                                }
+
+                                if (!empty($total_deposit)) {
+                                    $total_d = array_sum($total_deposit);
+                                } else {
+                                    $total_d = 0;
                                 }
                                 ?>
-                            <?php } ?>
-
-                            <?php
-                            if (!empty($total_payment)) {
-                                $total_p = array_sum($total_payment);
-                            } else {
-                                $total_p = 0;
-                            }
-
-                            if (!empty($total_deposit)) {
-                                $total_d = array_sum($total_deposit);
-                            } else {
-                                $total_d = 0;
-                            }
-                            ?>
-
-                            <tr class="total">
-                                <td></td>
-                                <td> <strong> <?php echo lang('total'); ?> </strong></td>
-                                <td> <strong> <?php echo $settings->currency; ?> <?php echo $total_p; ?> </strong></td>
-                                <td> <strong> <?php echo $settings->currency; ?> <?php echo $total_d; ?> </strong></td>
-
-                            </tr>
-
-                        </tbody>
-                    </table>
+                                <tr class="total">
+                                    <td></td>
+                                    <td> <strong> <?php echo lang('total'); ?> </strong></td>
+                                    <td> <strong> <?php echo $settings->currency; ?> <?php echo $total_p; ?> </strong></td>
+                                    <td> <strong> <?php echo $settings->currency; ?> <?php echo $total_d; ?> </strong></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
 
             <div class="col-md-4">
-
-
                 <?php
                 $total_bill = array();
                 foreach ($payments as $payment) {
@@ -345,7 +336,6 @@
                     $total_bill = 0;
                 }
                 ?>
-
                 <?php
                 $total_bill_ot = array();
                 if (!empty($total_bill_ot)) {
@@ -354,10 +344,6 @@
                     $total_bill_ot = 0;
                 }
                 ?>
-
-
-
-
                 <section class="panel">
                     <div class="weather-bg">
                         <div class="panel-body">
@@ -365,7 +351,6 @@
                                 <div class="col-xs-4">
                                     <i class="fa fa-money"></i>
                                     <?php echo lang('total_bill_amount'); ?>
-
                                 </div>
                                 <div class="col-xs-8">
                                     <div class="degree">
@@ -377,7 +362,6 @@
                         </div>
                     </div>
                 </section>
-
                 <section class="panel">
                     <div class="weather-bg">
                         <div class="panel-body">
