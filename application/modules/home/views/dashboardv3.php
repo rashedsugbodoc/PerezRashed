8bad46c9b91de64602fe83ea84009a83c77cba1a
Ryan Michael Gelig <ryan.gelig@gmail.com>
2021-10-18 02:05:41 +0800

    Fixed unclickable Subscription link in new sidebar menu new UI

--- application/modules/home/views/dashboardv3.php
+++ application/modules/home/views/dashboardv3.php
@@ -840,7 +840,7 @@ if (!$this->ion_auth->in_group(array('superadmin'))) {
                         </li>
                         <?php if ($this->ion_auth->in_group('admin')) { ?>
                             <li class="slide">
-                                <a class="side-menu__item"  data-toggle="slide" href="settings/subscription">
+                                <a class="side-menu__item" href="settings/subscription">
                                 <svg class="side-menu__icon" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M4 20h16v-8H4v8zm6-7.27L16 16l-6 3.26v-6.53z" opacity="1"/><path fill="currentColor" d="M4 6h16v2H4zm2-4h12v2H6zm14 8H4c-1.1 0-2 .9-2 2v8c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2v-8c0-1.1-.9-2-2-2zm0 10H4v-8h16v8zm-10-7.27v6.53L16 16z"/></svg>
                                 <span class="side-menu__label"><?php echo lang('subscription'); ?></span></a>
                             </li>
