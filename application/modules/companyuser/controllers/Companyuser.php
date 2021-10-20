ca72e02967a43d7fe385b96de21cf18153563977
Ryan Michael Gelig <ryan.gelig@gmail.com>
2021-10-20 21:47:42 +0800

    Renamed class name from CompanyUser to Companyuser

--- application/modules/companyuser/controllers/Companyuser.php
+++ application/modules/companyuser/controllers/Companyuser.php
@@ -3,7 +3,7 @@
 if (!defined('BASEPATH'))
     exit('No direct script access allowed');
 
-class CompanyUser extends MX_Controller {
+class Companyuser extends MX_Controller {
 
     function __construct() {
         parent::__construct();
