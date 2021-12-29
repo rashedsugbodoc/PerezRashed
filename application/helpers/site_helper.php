d30f2e8c6c3e26f265323a3282622e81e7a8f97c
kenneth <kenneth.rygeltech@gmail.com>
2021-12-29 17:57:55 +0800

    Update site_helper by adding computeAge method

--- application/helpers/site_helper.php
+++ application/helpers/site_helper.php
@@ -13,4 +13,16 @@ function getDoctorInfo() {
     function format_number_with_digits($number, $digits) {
         return substr(str_repeat(0, $digits).$number, - $digits);
     }
+
+    function computeAge($birthDateString) {
+
+        $birthDate = strtotime($birthDateString);
+        $birthDate = date('m/d/Y', $birthDate);
+        $birthDate = explode("/", $birthDate);
+        $year = (date("md", date("U", mktime(0, 0, 0, $birthDate[0], $birthDate[1], $birthDate[2]))) > date("md") ? ((date("Y") - $birthDate[2]) - 1) : (date("Y") - $birthDate[2]));
+        $month = (date("md", date("U", mktime(0, 0, 0, $birthDate[0], $birthDate[1], $birthDate[2]))) > date("md") ? ((date("m") - $birthDate[0]) - 1) : (date("m") - $birthDate[0]));
+
+        return $year;
+        
+    }
 ?>
