3c32ea589e6707a4f8acd385789fa59c059c07cd
kenneth <kenneth.rygeltech@gmail.com>
2022-01-07 18:21:16 +0800

    Update Country_models by changing table from country to countries

--- application/modules/country/models/Country_model.php
+++ application/modules/country/models/Country_model.php
@@ -11,29 +11,29 @@ class Country_model extends CI_model {
     }
 
     function insertCountry($data) {
-        $this->db->insert('country',$data);
+        $this->db->insert('countries',$data);
     }
 
     function getCountry() {
         $this->db->order_by('id', 'asc');
-        $query = $this->db->get('country');
+        $query = $this->db->get('countries');
         return $query->result();
     }
 
     function getCountryById($id) {
         $this->db->where('id', $id);
-        $query = $this->db->get('country');
+        $query = $this->db->get('countries');
         return $query->row();
     }
 
     function updateCountry($id, $data) {
         $this->db->where('id', $id);
-        $this->db->update('country', $data);
+        $this->db->update('countries', $data);
     }
 
     function delete($id) {
         $this->db->where('id', $id);
-        $this->db->delete('country');
+        $this->db->delete('countries');
     }
 
 }
