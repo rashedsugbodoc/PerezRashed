6b16b82fe780c4a7aa9458dbf55ee38381b8f6c2
Ryan Michael Gelig <ryan.gelig@gmail.com>
2022-02-19 14:29:32 +0800

    Created new view (edit_uploadv2) with functionality to edit an uploaded image using image editor plugin

---
+++ application/modules/patient/views/edit_uploadv2.php
@@ -0,0 +1,198 @@
+<!--html-->
+    <!--body-->
+        <!--div class="page"-->
+            <!--div class="page-main"-->
+                <!--div class="app-content main-content"-->
+                    <!--div class="side-app"-->
+                        <!--Page header-->
+
+                        <div class="page-header d-print-none">
+                            <div class="page-leftheader">
+                                <h4 class="page-title"><?php echo lang('edit');?> <?php echo lang('image');?> (<?php echo $document->title; ?>)</h4>
+                            </div>
+                        </div>
+                        <!--End Page header-->
+
+                        <!-- Row for Image Editor-->
+                        <div class="row mt-5">
+                            <div class="col-md-12">
+                                <div class="border border-light border-wd-1 ">
+                                    <pixie-editor></pixie-editor>
+                                </div>
+                                
+                            </div>
+                            
+                        </div>
+
+
+                        <?php
+                        $current_user = $this->ion_auth->get_user_id();
+                        if ($this->ion_auth->in_group('Doctor')) {
+                            $doctor_id = $this->db->get_where('doctor', array('ion_user_id' => $current_user))->row()->id;
+                        }
+                        ?>
+
+                    </div>
+                </div>
+            </div>
+
+            <!--Footer-->
+            <footer class="footer">
+                <div class="container">
+                    <div class="row align-items-center flex-row-reverse">
+                        <div class="col-md-12 col-sm-12 mt-3 mt-lg-0 text-center">
+                            Copyright © 2021 <a href="#">Rygel Dash</a>. Deployed by <a href="#">Rygel Technology Solutions</a> All rights reserved.
+                        </div>
+                    </div>
+                </div>
+            </footer>
+            <!-- End Footer-->
+        </div>
+
+        <!-- Back to top -->
+        <a href="#top" id="back-to-top">
+            <svg class="svg-icon" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M4 12l1.41 1.41L11 7.83V20h2V7.83l5.58 5.59L20 12l-8-8-8 8z"/></svg>
+        </a>
+
+
+    <!-- INTERNAL JS INDEX START -->
+        <!-- Jquery js-->
+        <script src="<?php echo base_url('public/assets/js/vendors/jquery-3.5.1.min.js'); ?>"></script>
+
+        <!-- Bootstrap4 js-->
+        <script src="<?php echo base_url('public/assets/plugins/bootstrap/popper.min.js'); ?>"></script>
+        <script src="<?php echo base_url('public/assets/plugins/bootstrap/js/bootstrap.min.js'); ?>"></script>
+
+        <!-- Circle-progress js-->
+        <script src="<?php echo base_url('public/assets/js/vendors/circle-progress.min.js'); ?>"></script>
+
+        <!--Sidemenu js-->
+        <script src="<?php echo base_url('public/assets/plugins/sidemenu/sidemenu.js'); ?>"></script>
+
+        <!-- P-scroll js-->
+        <script src="<?php echo base_url('public/assets/plugins/p-scrollbar/p-scrollbar.js'); ?>"></script>
+        <script src="<?php echo base_url('public/assets/plugins/p-scrollbar/p-scroll1.js'); ?>"></script>
+
+        <!-- Custom js-->
+        <script src="<?php echo base_url('public/assets/js/custom.js'); ?>"></script>
+
+        <!--Moment js-->
+        <script src="<?php echo base_url('public/assets/plugins/moment/moment.js') ?>"></script>
+
+        <!-- Daterangepicker js-->
+        <script src="<?php echo base_url('public/assets/plugins/bootstrap-daterangepicker/daterangepicker.js') ?>"></script>
+        <script src="<?php echo base_url('public/assets/js/daterange.js') ?>"></script>
+
+        <!--Select2 js -->
+        <script src="<?php echo base_url('public/assets/plugins/select2/select2.full.min.js'); ?>"></script>
+        <script src="<?php echo base_url('public/assets/js/select2.js'); ?>"></script>
+
+        <!-- Multiple select js -->
+        <script src="<?php echo base_url('public/assets/plugins/multipleselect/multiple-select.js'); ?>"></script>
+        <script src="<?php echo base_url('public/assets/plugins/multipleselect/multi-select.js'); ?>"></script>
+
+        <!--Sumoselect js-->
+        <script src="<?php echo base_url('public/assets/plugins/sumoselect/jquery.sumoselect.js'); ?>"></script>
+
+        <!--intlTelInput js-->
+        <script src="<?php echo base_url('public/assets/plugins/intl-tel-input-master/intlTelInput.js'); ?>"></script>
+        <script src="<?php echo base_url('public/assets/plugins/intl-tel-input-master/country-select.js'); ?>"></script>
+        <script src="<?php echo base_url('public/assets/plugins/intl-tel-input-master/utils.js'); ?>"></script>
+
+        <!--jquery transfer js-->
+        <script src="<?php echo base_url('public/assets/plugins/jQuerytransfer/jquery.transfer.js'); ?>"></script>
+
+        <!--multi js-->
+        <script src="<?php echo base_url('public/assets/plugins/multi/multi.min.js'); ?>"></script>
+
+        <!-- Notifications js -->
+        <script src="<?php echo base_url('public/assets/plugins/notify/js/rainbow.js'); ?>"></script>
+        <script src="<?php echo base_url('public/assets/plugins/notify/js/sample.js'); ?>"></script>
+        <script src="<?php echo base_url('public/assets/plugins/notify/js/jquery.growl.js'); ?>"></script>
+        <script src="<?php echo base_url('public/assets/plugins/notify/js/notifIt.js'); ?>"></script>
+
+        <!-- Image Editor-->
+        <script src="<?php echo base_url('public/assets/plugins/image-editor/scripts.min.js'); ?>"></script>
+
+        <script>
+            var pixie = new Pixie({
+                baseUrl: '<?php echo base_url('public/assets/plugins/image-editor'); ?>',
+                image: '<?php echo $document->url; ?>',
+                onLoad: function() {
+                    console.log('Pixie is ready');
+                },
+                onSave: function(data, name) {
+                    pixie.http().post('patient/saveUploadEditChanges', {name: name, data: data, id: '<?php echo $document->id;?>' }).subscribe(function(response) {
+                        console.log(response);
+                    });
+                },
+            });
+        </script>
+    <!-- INTERNAL JS INDEX END -->
+
+
+    <script>
+        $(document).ready(function () {
+
+            var error = "<?php echo $_SESSION['error'] ?>";
+            var success = "<?php echo $_SESSION['success'] ?>";
+            var notice = "<?php echo $_SESSION['notice'] ?>";
+            var warning = "<?php echo $_SESSION['warning'] ?>";
+
+            if (success) {
+                return $.growl.success({
+                    message: success
+                });
+            }
+            if (error) {
+                return $.growl.error({
+                    message: error
+                });
+            }
+            if (warning) {
+                return $.growl.warning({
+                    message: warning
+                });
+            }
+            if (notice) {
+                return $.growl.notice({
+                    message: notice
+                });
+            }
+
+            var error = "<?php unset($_SESSION['error']); ?>";
+            var success = "<?php unset($_SESSION['success']); ?>";
+            var warning = "<?php unset($_SESSION['warning']); ?>";
+            var notice = "<?php unset($_SESSION['notice']); ?>";
+
+        });
+    </script>
+
+    <script type="text/javascript">
+            
+        var uploadField = document.getElementById("document");
+
+        uploadField.onchange = function() {
+            if(this.files[0].size > 2e+6){
+               not2();
+               this.value = "";
+            }else{
+
+            }
+        };
+
+    </script>
+
+    <script type="text/javascript">
+        $(document).ready(function(){
+          $('.searchbox-input').on("keyup", function() {
+            var value = $(this).val().toLowerCase();
+            $(".col-xl-3").filter(function() {
+              $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+            });
+          });
+        });
+    </script>
+
+    </body>
+</html>
