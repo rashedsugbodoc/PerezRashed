b6d32b2c87c1736fcd368f0cae19d3281517e78f
kenneth <kenneth.rygeltech@gmail.com>
2022-02-25 16:34:02 +0800

    Update site_helper by adding new function time_elapsed_string

--- application/helpers/site_helper.php
+++ application/helpers/site_helper.php
@@ -81,4 +81,34 @@ function getDoctorInfo() {
 
         return $mg;
     }
+
+    function time_elapsed_string($datetime, $level = 7) {
+        $now = new DateTime;
+        $ago = new DateTime($datetime);
+        $diff = $now->diff($ago);
+
+        $diff->w = floor($diff->d / 7);
+        $diff->d -= $diff->w * 7;
+
+        $string = array(
+            'y' => 'year',
+            'm' => 'month',
+            'w' => 'week',
+            'd' => 'day',
+            'h' => 'hour',
+            'i' => 'minute',
+            's' => 'second',
+        );
+        foreach ($string as $k => &$v) {
+            if ($diff->$k) {
+                $v = $diff->$k . ' ' . $v . ($diff->$k > 1 ? 's' : '');
+            } else {
+                unset($string[$k]);
+            }
+        }
+
+        $string = array_slice($string, 0, $level);
+        return $string ? implode(', ', $string) . ' ago' : 'just now';
+    }
+
 ?>
