22f9ace0fb0f8ddc608423b73994fb9151523f94
Ryan Michael Gelig <ryan.gelig@gmail.com>
2022-03-06 10:32:08 +0800

    Replace long code to access the img_url for each user group table and just assign the session variable profile_img_url to display profile pic

--- application/modules/home/views/dashboardv2.php
+++ application/modules/home/views/dashboardv2.php
@@ -1334,50 +1334,7 @@ if (!$this->ion_auth->in_group(array('superadmin'))) {
                                                 <span>
                                                     <?php
                                                         $user = $this->session->userdata('user_id');
-
-                                                        if ($this->ion_auth->in_group('Doctor')) {
-                                                            $user_image = $this->doctor_model->getDoctorByIonUserId($user)->img_url;
-                                                            if (empty($user_image)) {
-                                                                $user_image = 'public/assets/images/users/16.jpg';
-                                                            }
-                                                        } else if ($this->ion_auth->in_group('Nurse')) {
-                                                            $user_image = $this->nurse_model->getNurseByIonUserId($user)->img_url;
-                                                            if (empty($user_image)) {
-                                                                $user_image = 'public/assets/images/users/16.jpg';
-                                                            }
-                                                        } else if ($this->ion_auth->in_group('Pharmacist')) {
-                                                            $user_image = $this->pharmacist_model->getPharmacistByIonUserId($user)->img_url;
-                                                            if (empty($user_image)) {
-                                                                $user_image = 'public/assets/images/users/16.jpg';
-                                                            }
-                                                        } else if ($this->ion_auth->in_group('Laboratorist')) {
-                                                            $user_image = $this->laboratorist_model->getLaboratoristByIonUserId($user)->img_url;
-                                                            if (empty($user_image)) {
-                                                                $user_image = 'public/assets/images/users/16.jpg';
-                                                            }
-                                                        } else if ($this->ion_auth->in_group('Accountant')) {
-                                                            $user_image = $this->accountant_model->getAccountantByIonUserId($user)->img_url;
-                                                            if (empty($user_image)) {
-                                                                $user_image = 'public/assets/images/users/16.jpg';
-                                                            }
-                                                        } else if ($this->ion_auth->in_group('Receptionist')) {
-                                                            $user_image = $this->receptionist_model->getReceptionistByIonUserId($user)->img_url;
-                                                            if (empty($user_image)) {
-                                                                $user_image = 'public/assets/images/users/16.jpg';
-                                                            }
-                                                        } else if ($this->ion_auth->in_group('CompanyUser')) {
-                                                            $user_image = $this->companyuser_model->getCompanyUserByIonUserId($user)->img_url;
-                                                            if (empty($user_image)) {
-                                                                $user_image = 'public/assets/images/users/16.jpg';
-                                                            }
-                                                        } else if ($this->ion_auth->in_group('Patient')) {
-                                                            $user_image = $this->patient_model->getPatientByIonUserId($user)->img_url;
-                                                            if (empty($user_image)) {
-                                                                $user_image = 'public/assets/images/users/16.jpg';
-                                                            }
-                                                        } else {
-                                                            $user_image = 'public/assets/images/users/16.jpg';
-                                                        }
+                                                        $user_image = $this->session->userdata('profile_img_url');
                                                     ?>
                                                     <img src="<?php echo base_url($user_image); ?>" alt="img" class="avatar avatar-md brround">
                                                 </span>
