cd00374592ba9faab70821d941a5998d85d15d4b
Ryan Michael Gelig <ryan.gelig@gmail.com>
2022-03-14 17:51:00 +0800

    Modify Auth Controller by adding csrf_token_name and csrf_token_hash variables to login method. Also added new register method for registering new Patient. Unfinished code for registration

--- application/modules/auth/controllers/Auth.php
+++ application/modules/auth/controllers/Auth.php
@@ -8,6 +8,7 @@ class Auth extends MX_Controller {
 		$this->load->database();
 		$this->load->library(array('ion_auth','form_validation'));
 		$this->load->helper(array('url','language'));
+		$this->load->model('patient/patient_model');
 
 		$this->form_validation->set_error_delimiters($this->config->item('error_start_delimiter', 'ion_auth'), $this->config->item('error_end_delimiter', 'ion_auth'));
 
@@ -49,6 +50,8 @@ class Auth extends MX_Controller {
 			redirect('home');
 		}
 		$data['title'] = "Login";
+		$data['csrf_token_name'] = $this->security->get_csrf_token_name();
+        $data['csrf_token_hash'] = $this->security->get_csrf_hash();
 
 		//validate form input
 		$this->form_validation->set_rules('identity', 'Identity', 'required');
@@ -417,6 +420,103 @@ class Auth extends MX_Controller {
 
 			//redirect them back to the auth page
 			redirect('auth', 'refresh');
+		}
+	}
+
+	//create a new user
+	function register()
+	{
+		$data['title'] = "Create User";
+		$data['civil_status'] = $this->patient_model->getCivilStatus();
+		$data['blood_groups'] = $this->patient_model->getBloodGroup();
+
+		if ($this->ion_auth->logged_in())
+		{
+			//redirect('auth', 'refresh');
+			$this->ion_auth->logout();
+		}
+
+		$tables = $this->config->item('tables','ion_auth');
+
+		//validate form input
+		$this->form_validation->set_rules('first_name', $this->lang->line('create_user_validation_fname_label'), 'required');
+		$this->form_validation->set_rules('last_name', $this->lang->line('create_user_validation_lname_label'));
+		$this->form_validation->set_rules('email', $this->lang->line('create_user_validation_email_label'), 'required|valid_email|is_unique['.$tables['users'].'.email]');
+		$this->form_validation->set_rules('phone', $this->lang->line('create_user_validation_phone_label'), 'required');
+		$this->form_validation->set_rules('company', $this->lang->line('create_user_validation_company_label'));
+		$this->form_validation->set_rules('password', $this->lang->line('create_user_validation_password_label'), 'required|min_length[' . $this->config->item('min_password_length', 'ion_auth') . ']|max_length[' . $this->config->item('max_password_length', 'ion_auth') . ']|matches[password_confirm]');
+		$this->form_validation->set_rules('password_confirm', $this->lang->line('create_user_validation_password_confirm_label'), 'required');
+
+		if ($this->form_validation->run() == true)
+		{
+			$username = strtolower($this->input->post('first_name')) . ' ' . strtolower($this->input->post('last_name'));
+			$email    = strtolower($this->input->post('email'));
+			$password = $this->input->post('password');
+
+			$additional_data = array(
+				'first_name' => $this->input->post('first_name'),
+				'last_name'  => $this->input->post('last_name'),
+				'company'    => $this->input->post('company'),
+				'phone'      => $this->input->post('phone'),
+			);
+		}
+		if ($this->form_validation->run() == true && $this->ion_auth->register($username, $password, $email, $additional_data))
+		{
+			//check to see if we are creating the user
+			//redirect them back to the admin page
+			$this->session->set_flashdata('message', $this->ion_auth->messages());
+			redirect("auth", 'refresh');
+		}
+		else
+		{
+			//display the create user form
+			//set the flash data error message if there is one
+			$data['message'] = (validation_errors() ? validation_errors() : ($this->ion_auth->errors() ? $this->ion_auth->errors() : $this->session->flashdata('message')));
+
+			$data['first_name'] = array(
+				'name'  => 'first_name',
+				'id'    => 'first_name',
+				'type'  => 'text',
+				'value' => $this->form_validation->set_value('first_name'),
+			);
+			$data['middle_name'] = array(
+				'name'  => 'middle_name',
+				'id'    => 'middle_name',
+				'type'  => 'text',
+				'value' => $this->form_validation->set_value('last_name'),
+			);
+			$data['last_name'] = array(
+				'name'  => 'last_name',
+				'id'    => 'last_name',
+				'type'  => 'text',
+				'value' => $this->form_validation->set_value('last_name'),
+			);
+			$data['email'] = array(
+				'name'  => 'email',
+				'id'    => 'email',
+				'type'  => 'email',
+				'value' => $this->form_validation->set_value('email'),
+			);
+			$data['mobile_number'] = array(
+				'name'  => 'mobile',
+				'id'    => 'mobile',
+				'type'  => 'text',
+				'value' => $this->form_validation->set_value('mobile_number'),
+			);
+			$data['password'] = array(
+				'name'  => 'password',
+				'id'    => 'password',
+				'type'  => 'password',
+				'value' => $this->form_validation->set_value('password'),
+			);
+			$data['password_confirm'] = array(
+				'name'  => 'password_confirm',
+				'id'    => 'password_confirm',
+				'type'  => 'password',
+				'value' => $this->form_validation->set_value('password_confirm'),
+			);
+
+			$this->_render_page('auth/register', $data);
 		}
 	}
 
