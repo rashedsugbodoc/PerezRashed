51fde6263b82ec79bb564f65ee583610c2d8bb41
kenneth <kenneth.rygeltech@gmail.com>
2022-03-17 11:51:09 +0800

    Add Labrequest Link on Dashboards Sidebar Menu

--- application/modules/home/views/dashboardv2.php
+++ application/modules/home/views/dashboardv2.php
@@ -495,6 +495,16 @@ if (!$this->ion_auth->in_group(array('superadmin'))) {
                             <?php } ?>
                         <?php } ?>
 
+                        <?php if ($this->ion_auth->in_group(array('admin', 'Doctor'))) { ?>
+                            <?php if (in_array('labrequest', $this->modules)) { ?>
+                                <li class="slide">
+                                    <a class="side-menu__item"  href="labrequest">
+                                    <svg class="side-menu__icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" width="24px" height="24px"><path fill="currentColor" d="M301.26 352l78.06-78.06c6.25-6.25 6.25-16.38 0-22.63l-22.63-22.63c-6.25-6.25-16.38-6.25-22.63 0L256 306.74l-83.96-83.96C219.31 216.8 256 176.89 256 128c0-53.02-42.98-96-96-96H16C7.16 32 0 39.16 0 48v256c0 8.84 7.16 16 16 16h32c8.84 0 16-7.16 16-16v-80h18.75l128 128-78.06 78.06c-6.25 6.25-6.25 16.38 0 22.63l22.63 22.63c6.25 6.25 16.38 6.25 22.63 0L256 397.25l78.06 78.06c6.25 6.25 16.38 6.25 22.63 0l22.63-22.63c6.25-6.25 6.25-16.38 0-22.63L301.26 352zM64 96h96c17.64 0 32 14.36 32 32s-14.36 32-32 32H64V96z"></path></svg>
+                                    <span class="side-menu__label"><?php echo lang('lab') . ' ' . lang('requests'); ?></span></a>
+                                </li>
+                            <?php } ?>
+                        <?php } ?>
+
                         <?php if ($this->ion_auth->in_group(array('Receptionist', 'Nurse', 'Doctor'))) { ?>
                             <?php if (in_array('lab', $this->modules)) { ?>
                                 <li class="slide">
