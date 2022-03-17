e80514a0f2e4ce430458685e1e48c67f04806136
kenneth <kenneth.rygeltech@gmail.com>
2022-03-17 12:03:00 +0800

    Add Labrequest File

---
+++ application/modules/labrequest/views/labrequest.php
@@ -0,0 +1,175 @@
+<!--html-->
+    <!--body-->
+        <!--div class="page"-->
+            <!--div class="page-main"-->
+                <!--div class="app-content main-content"-->
+                    <!--div class="side-app"-->
+                        <!--Page header-->
+
+                        <div class="row mt-5">
+                            <div class="col-md-12 col-sm-12">
+                                <div class="card">
+                                    <div class="card-header">
+                                        <div class="card-title">
+                                            <?php echo lang("lab") . ' ' . lang("requests"); ?>
+                                        </div>
+                                        <div class="card-options">
+                                            <a href="labrequest/addLabRequestView" class="btn btn-primary"><?php echo lang('add') . ' ' . lang("lab") . ' ' . lang("requests"); ?></a>
+                                        </div>
+                                    </div>
+                                    <div class="card-body">
+                                        <div class="">
+                                            <div class="table-responsive">
+                                                <table class="table table-bordered text-nowrap key-buttons" id="editable-sample">
+                                                    <thead>
+                                                        <tr>
+                                                            <th class="border-bottom-0"><?php echo lang('id'); ?></th>
+                                                            <th class="border-bottom-0"><?php echo lang('loinc_num'); ?></th>
+                                                            <th class="border-bottom-0"><?php echo lang('patient'); ?></th>
+                                                            <th class="border-bottom-0"><?php echo lang('doctors'); ?></th>
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody>
+                                                        
+                                                    </tbody>
+                                                </table>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+            <!--Footer-->
+            <footer class="footer">
+                <div class="container">
+                    <div class="row align-items-center flex-row-reverse">
+                        <div class="col-md-12 col-sm-12 mt-3 mt-lg-0 text-center">
+                            Copyright © 2021 <a href="#">Rygel Dash</a>. Deployed by <a href="#">Rygel Technology Solutions</a> All rights reserved.
+                        </div>
+                    </div>
+                </div>
+            </footer>
+            <!-- End Footer-->
+        </div>
+
+    <!-- INTERNAL JS INDEX START -->
+        <!-- Jquery js-->
+        <script src="<?php echo base_url('public/assets/js/vendors/jquery-3.5.1.min.js'); ?>"></script>
+
+        <!-- Bootstrap4 js-->
+        <script src="<?php echo base_url('public/assets/plugins/bootstrap/popper.min.js'); ?>"></script>
+        <script src="<?php echo base_url('public/assets/plugins/bootstrap/js/bootstrap.min.js'); ?>"></script>
+
+        <!--Othercharts js-->
+        <script src="<?php echo base_url('public/assets/plugins/othercharts/jquery.sparkline.min.js'); ?>"></script>
+
+        <!-- Circle-progress js-->
+        <script src="<?php echo base_url('public/assets/js/vendors/circle-progress.min.js'); ?>"></script>
+
+        <!-- Jquery-rating js-->
+        <script src="<?php echo base_url('public/assets/plugins/rating/jquery.rating-stars.js'); ?>"></script>
+
+        <!--Sidemenu js-->
+        <script src="<?php echo base_url('public/assets/plugins/sidemenu/sidemenu.js'); ?>"></script>
+
+        <!-- P-scroll js-->
+        <script src="<?php echo base_url('public/assets/plugins/p-scrollbar/p-scrollbar.js'); ?>"></script>
+        <script src="<?php echo base_url('public/assets/plugins/p-scrollbar/p-scroll1.js'); ?>"></script>
+
+        <!-- Custom js-->
+        <script src="<?php echo base_url('public/assets/js/custom.js'); ?>"></script>
+
+        <!--Moment js-->
+        <script src="<?php echo base_url('public/assets/plugins/moment/moment.js') ?>"></script>
+
+        <!-- Data tables js-->
+        <script src="<?php echo base_url('public/assets/plugins/datatable/js/jquery.dataTables.js') ?>"></script>
+        <script src="<?php echo base_url('public/assets/plugins/datatable/js/dataTables.bootstrap4.js') ?>"></script>
+        <script src="<?php echo base_url('public/assets/plugins/datatable/js/dataTables.buttons.min.js') ?>"></script>
+        <script src="<?php echo base_url('public/assets/plugins/datatable/js/buttons.bootstrap4.min.js') ?>"></script>
+        <script src="<?php echo base_url('public/assets/plugins/datatable/js/jszip.min.js') ?>"></script>
+        <script src="<?php echo base_url('public/assets/plugins/datatable/js/pdfmake.min.js') ?>"></script>
+        <script src="<?php echo base_url('public/assets/plugins/datatable/js/vfs_fonts.js') ?>"></script>
+        <script src="<?php echo base_url('public/assets/plugins/datatable/js/buttons.html5.min.js') ?>"></script>
+        <script src="<?php echo base_url('public/assets/plugins/datatable/js/buttons.print.min.js') ?>"></script>
+        <script src="<?php echo base_url('public/assets/plugins/datatable/js/buttons.colVis.min.js') ?>"></script>
+        <script src="<?php echo base_url('public/assets/plugins/datatable/dataTables.responsive.min.js') ?>"></script>
+        <script src="<?php echo base_url('public/assets/plugins/datatable/responsive.bootstrap4.min.js') ?>"></script>
+        <script src="<?php echo base_url('public/assets/js/datatables.js') ?>"></script>
+
+        <!-- Notifications js -->
+        <script src="<?php echo base_url('public/assets/plugins/notify/js/rainbow.js'); ?>"></script>
+        <script src="<?php echo base_url('public/assets/plugins/notify/js/sample.js'); ?>"></script>
+        <script src="<?php echo base_url('public/assets/plugins/notify/js/jquery.growl.js'); ?>"></script>
+        <script src="<?php echo base_url('public/assets/plugins/notify/js/notifIt.js'); ?>"></script>
+
+    <!-- INTERNAL JS INDEX END -->
+
+    <script>
+        $(document).ready(function () {
+            var table = $('#editable-sample').DataTable({
+                responsive: true,
+                //   dom: 'lfrBtip',
+
+                
+                "ajax": {
+                    url: "labrequest/getLabrequest",
+                    type: 'POST',
+                },
+                scroller: {
+                    loadingIndicator: true
+                },
+                dom: "<'row'<'col-sm-4 col-md-12 col-lg-12'f><'col-sm-12 col-md-12 col-lg-2'l><'col-sm-12 col-md-12 col-lg-10 text-right'B>>" +
+                        "<'row'<'col-sm-12'tr>>" +
+                        "<'row'<'col-sm-5'i><'col-sm-7'p>>",
+                buttons: [
+                    {
+                        extend: 'excelHtml5',
+                        exportOptions: {
+                            columns: [0, 1, 2, 3],
+                        }
+                    },
+                    {
+                        extend: 'csvHtml5',
+                        exportOptions: {
+                            columns: [0, 1, 2, 3],
+                        }
+                    },
+                    {
+                        extend: 'pdfHtml5',
+                        exportOptions: {
+                            columns: [0, 1, 2, 3],
+                        }
+                    },
+                    {
+                        extend: 'print',
+                        exportOptions: {
+                            columns: [0, 1, 2],
+                        }
+                    },
+                ],
+                aLengthMenu: [
+                    [10, 25, 50, 100, -1],
+                    [10, 25, 50, 100, "All"]
+                ],
+                iDisplayLength: 100,
+                "order": [[0, "desc"]],
+
+                "language": {
+                    "lengthMenu": "_MENU_",
+                    search: "_INPUT_",
+                    "url": "common/assets/DataTables/languages/<?php echo $this->language; ?>.json"
+                }
+            });
+            table.buttons().container().appendTo('.custom_buttons');
+        });
+
+    </script>
+
+    </body>
+</html>
