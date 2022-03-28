161f41dbfabbde865667c577867aaa661e0192ef
Ryan Michael Gelig <ryan.gelig@gmail.com>
2022-03-29 02:04:46 +0800

    Modify dashboardv2 by changing Flatpickr datatime plugin theme to sugbodoc_purple.css

--- application/modules/home/views/dashboardv2.php
+++ application/modules/home/views/dashboardv2.php
@@ -157,7 +157,7 @@ if (!$this->ion_auth->in_group(array('superadmin'))) {
 
         <!-- FlatPicker css -->
         <link href="<?php echo base_url('common/assets/flatpickr/dist/flatpickr.min.css'); ?>" rel="stylesheet">
-        <link href="<?php echo base_url('common/assets/flatpickr/dist/themes/material_blue.css'); ?>" rel="stylesheet">
+        <link href="<?php echo base_url('common/assets/flatpickr/dist/themes/sugbodoc_purple.css'); ?>" rel="stylesheet">
 
         <!-- INTERNAL CSS END -->
 
