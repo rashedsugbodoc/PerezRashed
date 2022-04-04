5676bfe03cc259f7ffcbd67db330c15ff4dfeb7f
kenneth <kenneth.rygeltech@gmail.com>
2022-04-04 17:49:03 +0800

    Update Encounter Add Vital Modal by removing FullCalendar Date and Time Field and Replace with Flatpickr DateTime with a name of datetime

--- application/modules/encounter/views/encounter.php
+++ application/modules/encounter/views/encounter.php
@@ -376,13 +376,13 @@
                                     <form role="form" id="addVitalForm" action="patient/addVitals" class="clearfix" method="post" enctype="multipart/form-data">
                                         <div class="modal-body">
                                             <div class="row">
-                                                <div class="col-md-6 col-sm-12">
+                                                <div class="col-md-12 col-sm-12">
                                                     <div class="form-group">
                                                         <label class="form-label"><?php echo lang('date'); ?> <?php echo lang('measured'); ?></label>
-                                                        <input class="form-control fc-datepicker" readonly name="date" placeholder="MM/DD/YYYY" type="text">
+                                                        <input class="form-control flatpickr" readonly name="datetime" placeholder="MM/DD/YYYY" type="text">
                                                     </div>
                                                 </div>
-                                                <div class="col-md-6 col-sm-12">
+                                                <!-- <div class="col-md-6 col-sm-12">
                                                     <div class="form-group">
                                                         <label class="form-label"><?php echo lang('time'); ?> <?php echo lang('measured'); ?></label>
                                                         <div class="wd-150 mg-b-30">
@@ -390,13 +390,13 @@
                                                                 <div class="input-group-prepend">
                                                                     <div class="input-group-text">
                                                                         <svg class="svg-icon" xmlns="http://www.w3.org/2000/svg" height="18" viewBox="0 0 24 24" width="18"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M12 4c-4.42 0-8 3.58-8 8s3.58 8 8 8 8-3.58 8-8-3.58-8-8-8zm4.25 12.15L11 13V7h1.5v5.25l4.5 2.67-.75 1.23z" opacity=".3"/><path d="M11.99 2C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8zm.5-13H11v6l5.25 3.15.75-1.23-4.5-2.67z"/></svg>
-                                                                    </div><!-- input-group-text -->
-                                                                </div><!-- input-group-prepend -->
+                                                                    </div>
+                                                                </div>
                                                                 <input class="form-control" id="tpBasic" name="time" placeholder="Set time" type="text">
                                                             </div>
-                                                        </div><!-- wd-150 -->
+                                                        </div>
                                                     </div>
-                                                </div>
+                                                </div> -->
                                             </div>
                                             <div class="row">
                                                 <div class="col-md-6 col-sm-12">
@@ -970,6 +970,9 @@
         <!-- quill js -->
         <script src="<?php echo base_url('public/assets/plugins/quill/quill.min.js'); ?>"></script>
         <script src="<?php echo base_url('public/assets/js/form-editor2.js'); ?>"></script>
+
+        <!-- flatpickr js -->
+        <script src="<?php echo base_url('common/assets/flatpickr/dist/flatpickr.js'); ?>"></script>
         <!-- INTERNAL JS INDEX END -->
 
     <!-- INTERNAL JS INDEX END -->
@@ -985,6 +988,20 @@
         });
 
     </script> -->
+
+    <script type="text/javascript">
+        $(document).ready(function () {
+            flatpickr(".flatpickr", {
+                maxDate: "today",
+                altInput: true,
+                altFormat: "F j, Y h:i K",
+                dateFormat: "Y-m-d h:i K",
+                disableMobile: "true",
+                enableTime: true,
+                defaultDate: "today",
+            });
+        });
+    </script>
 
     <script type="text/javascript">
         $(document).ready(function () {
