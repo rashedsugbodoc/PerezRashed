eaeaa98d43e97b01951655fee01dbdba994d9d52
kenneth <kenneth.rygeltech@gmail.com>
2022-04-14 13:59:43 +0800

    Update Pages Daily Expense, Daily, Monthly Expense by putting stock_alert inside a quote

--- application/modules/finance/views/daily_expensev2.php
+++ application/modules/finance/views/daily_expensev2.php
@@ -101,7 +101,7 @@
                                 <div class="modal-content">
                                     <div class="modal-header">
                                         <button type="button" class="close" data-dismiss="modal">&times;</button>
-                                        <h4 class="modal-title"><?php echo lang(stock_alert); ?></h4>
+                                        <h4 class="modal-title"><?php echo lang('stock_alert'); ?></h4>
                                     </div>
                                     <div class="modal-body">
 
