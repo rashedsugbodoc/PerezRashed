bfcb004907d35ffdc7410c5383016954712c6fb3
kenneth <kenneth.rygeltech@gmail.com>
2022-04-27 14:44:36 +0800

    Update Patient Prescription View by changing column from prescription->date to prescription->prescription_date Column to be use for Patient Prescription View

--- application/modules/patient/views/my_prescriptionv2.php
+++ application/modules/patient/views/my_prescriptionv2.php
@@ -34,7 +34,7 @@
                                             <tbody>
                                                 <?php foreach ($prescriptions as $prescription) { ?>
                                                     <tr class="">
-                                                        <td><?php echo date('d-m-Y', strtotime($prescription->date)); ?></td>
+                                                        <td><?php echo date('d-m-Y', strtotime($prescription->prescription_date.' UTC')); ?></td>
                                                         <td> <?php echo $this->patient_model->getPatientById($prescription->patient)->name; ?></td>
                                                         <td>
 
