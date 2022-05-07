a5b5f2d1eed7af5d7376403168c7d2fe62856276
kenneth <kenneth.rygeltech@gmail.com>
2022-05-07 16:43:23 +0800

    Update Dashboardv2 by adding if COndition for Mail Module to check if the Provider have mail module

--- application/modules/home/views/dashboardv2.php
+++ application/modules/home/views/dashboardv2.php
@@ -927,15 +927,17 @@ if (!$this->ion_auth->in_group(array('superadmin'))) {
                             </li>
                         <?php  } ?>
 
-                        <?php if (!$this->ion_auth->in_group(array('admin', 'Patient', 'superadmin'))) { ?>
-                            <li class="slide">
-                                <a class="side-menu__item" data-toggle="slide" href="javascript:;">
-                                <svg class="side-menu__icon" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M20 8l-8 5-8-5v10h16zm0-2H4l8 4.99z" opacity="1"/><path fill="currentColor" d="M4 20h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2zM20 6l-8 4.99L4 6h16zM4 8l8 5 8-5v10H4V8z"/></svg>
-                                <span class="side-menu__label"><?php echo lang('email'); ?></span><i class="angle fa fa-angle-right"></i></a>
-                                <ul class="slide-menu">
-                                    <li><a href="email/sendView" class="slide-item"><?php echo lang('new'); ?></a></li>
-                                </ul>
-                            </li>
+                        <?php if (in_array('email', $this->modules)) { ?>
+                            <?php if (!$this->ion_auth->in_group(array('admin', 'Patient', 'superadmin'))) { ?>
+                                <li class="slide">
+                                    <a class="side-menu__item" data-toggle="slide" href="javascript:;">
+                                    <svg class="side-menu__icon" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M20 8l-8 5-8-5v10h16zm0-2H4l8 4.99z" opacity="1"/><path fill="currentColor" d="M4 20h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2zM20 6l-8 4.99L4 6h16zM4 8l8 5 8-5v10H4V8z"/></svg>
+                                    <span class="side-menu__label"><?php echo lang('email'); ?></span><i class="angle fa fa-angle-right"></i></a>
+                                    <ul class="slide-menu">
+                                        <li><a href="email/sendView" class="slide-item"><?php echo lang('new'); ?></a></li>
+                                    </ul>
+                                </li>
+                            <?php } ?>
                         <?php } ?>
 
                         <?php if ($this->ion_auth->in_group('superadmin')) { ?>
