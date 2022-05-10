03537c0fe9418c03b553c363a5983ec1cacecea8
kenneth <kenneth.rygeltech@gmail.com>
2022-05-10 17:34:55 +0800

    Add New UI Language Setting PHP file

---
+++ application/modules/settings/views/languagev2.php
@@ -0,0 +1,272 @@
+<!--html-->
+    <!--body-->
+        <!--div class="page"-->
+            <!--div class="page-main"-->
+                <!--div class="app-content main-content"-->
+                    <!--div class="side-app"-->
+                        <!--Page header-->
+
+                        <div class="row mt-5">
+                            <div class="col-md-7 col-sm-12">
+                                <div class="card">
+                                    <div class="card-header">
+                                        <div class="card-title">
+                                            <?php echo lang('select'); ?> <?php echo lang('language'); ?>
+                                        </div>
+                                    </div>
+                                    <div class="card-body">
+                                        <form role="form" id="editSaleForm" action="settings/changeLanguage" method="post" enctype="multipart/form-data">  
+                                            <div class="row">
+                                                <div class="col-md-12 col-sm-12">
+                                                    <div class="form-group">
+                                                        <label class="form-label"><?php echo lang('language'); ?></label>
+                                                        <select class="form-control select2-show-search" name="language">
+                                                            <option value="arabic" <?php
+                                                            if (!empty($settings->language)) {
+                                                                if ($settings->language == 'arabic') {
+                                                                    echo 'selected';
+                                                                }
+                                                            }
+                                                            ?>><?php echo lang('arabic'); ?> 
+                                                            </option>
+                                                            <option value="english" <?php
+                                                            if (!empty($settings->language)) {
+                                                                if ($settings->language == 'english') {
+                                                                    echo 'selected';
+                                                                }
+                                                            }
+                                                            ?>><?php echo lang('english'); ?> 
+                                                            </option>
+                                                            <option value="spanish" <?php
+                                                            if (!empty($settings->language)) {
+                                                                if ($settings->language == 'spanish') {
+                                                                    echo 'selected';
+                                                                }
+                                                            }
+                                                            ?>><?php echo lang('spanish'); ?>
+                                                            </option>
+                                                            <option value="french" <?php
+                                                            if (!empty($settings->language)) {
+                                                                if ($settings->language == 'french') {
+                                                                    echo 'selected';
+                                                                }
+                                                            }
+                                                            ?>><?php echo lang('french'); ?>
+                                                            </option>
+                                                            <option value="italian" <?php
+                                                            if (!empty($settings->language)) {
+                                                                if ($settings->language == 'italian') {
+                                                                    echo 'selected';
+                                                                }
+                                                            }
+                                                            ?>><?php echo lang('italian'); ?>
+                                                            </option>
+                                                            <option value="portuguese" <?php
+                                                            if (!empty($settings->language)) {
+                                                                if ($settings->language == 'portuguese') {
+                                                                    echo 'selected';
+                                                                }
+                                                            }
+                                                            ?>><?php echo lang('portuguese'); ?>
+                                                            </option>
+                                                        </select>
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-12 col-sm-12">
+                                                    <input type="hidden" name="language_settings" value='language_settings'>
+                                                    <input type="hidden" name="id" value='<?php
+                                                    if (!empty($settings->id)) {
+                                                        echo $settings->id;
+                                                    }
+                                                    ?>'>
+                                                </div>
+                                                <div class="col-md-12 col-sm-12">
+                                                    <div class="form-group">
+                                                        <button type="submit" name="submit" class="btn btn-primary pull-right"> <?php echo lang('submit'); ?></button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+
+                                <?php if ($this->ion_auth->in_group('superadmin')) { ?>
+                                    <div class="card">
+                                        <div class="card-header">
+                                            <div class="card-title">
+                                                <?php echo lang('edit'); ?> <?php echo lang('language'); ?>
+                                            </div>
+                                        </div>
+                                        <div class="card-body">
+                                            <div class="">
+                                                <div class="table-responsive">
+                                                    <table id="editable-sample" class="table table-bordered text-nowrap key-buttons">
+                                                        <thead>
+                                                            <tr>
+                                                                <th>#</th>
+                                                                <th><?php echo lang('name'); ?></th>
+                                                                <th><?php echo lang('options'); ?></th>
+                                                            </tr>
+                                                        </thead>
+                                                        <tbody> 
+                                                            <?php
+                                                            $i = 0;
+                                                            foreach ($pgateways as $pgateway) {
+                                                                $i = $i + 1;
+                                                                ?>
+                                                                <tr>
+                                                                    <td><?php echo $i; ?></td>
+                                                                    <td><?php
+                                                                        if (!empty($pgateway->name)) {
+                                                                            echo $pgateway->name;
+                                                                        }
+                                                                        ?></td>
+
+                                                                    <td>
+                                                                        <a class="btn btn-info btn-xs" href="pgateway/settings?id=<?php echo $pgateway->id; ?>">   <?php echo lang('manage'); ?></a>
+                                                                    </td>
+                                                                </tr>
+                                                            <?php } ?>
+                                                        </tbody>
+                                                    </table>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                <?php } ?>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+            <!--Footer-->
+            <footer class="footer">
+                <div class="container">
+                    <div class="row align-items-center flex-row-reverse">
+                        <div class="col-md-12 col-sm-12 mt-3 mt-lg-0 text-center">
+                            Copyright © 2021 <a href="#">Rygel Dash</a>. Deployed by <a href="#">Rygel Technology Solutions</a> All rights reserved.
+                        </div>
+                    </div>
+                </div>
+            </footer>
+            <!-- End Footer-->
+        </div>
+
+    <!-- INTERNAL JS INDEX START -->
+
+        <!-- Jquery js-->
+        <script src="<?php echo base_url('public/assets/js/vendors/jquery-3.5.1.min.js'); ?>"></script>
+
+        <!-- Bootstrap4 js-->
+        <script src="<?php echo base_url('public/assets/plugins/bootstrap/popper.min.js'); ?>"></script>
+        <script src="<?php echo base_url('public/assets/plugins/bootstrap/js/bootstrap.min.js'); ?>"></script>
+
+        <!--Othercharts js-->
+        <script src="<?php echo base_url('public/assets/plugins/othercharts/jquery.sparkline.min.js'); ?>"></script>
+
+        <!-- Circle-progress js-->
+        <script src="<?php echo base_url('public/assets/js/vendors/circle-progress.min.js'); ?>"></script>
+
+        <!-- Jquery-rating js-->
+        <script src="<?php echo base_url('public/assets/plugins/rating/jquery.rating-stars.js'); ?>"></script>
+
+        <!--Sidemenu js-->
+        <script src="<?php echo base_url('public/assets/plugins/sidemenu/sidemenu.js'); ?>"></script>
+
+        <!-- P-scroll js-->
+        <script src="<?php echo base_url('public/assets/plugins/p-scrollbar/p-scrollbar.js'); ?>"></script>
+        <script src="<?php echo base_url('public/assets/plugins/p-scrollbar/p-scroll1.js'); ?>"></script>
+
+        <!-- Custom js-->
+        <script src="<?php echo base_url('public/assets/js/custom.js'); ?>"></script>
+
+        <!--Moment js-->
+        <script src="<?php echo base_url('public/assets/plugins/moment/moment.js') ?>"></script>
+
+        <!-- Daterangepicker js-->
+        <script src="<?php echo base_url('public/assets/plugins/bootstrap-daterangepicker/daterangepicker.js') ?>"></script>
+        <script src="<?php echo base_url('public/assets/js/daterange.js') ?>"></script>
+
+        <!--Select2 js -->
+        <script src="<?php echo base_url('public/assets/plugins/select2/select2.full.min.js'); ?>"></script>
+        <script src="<?php echo base_url('public/assets/js/select2.js'); ?>"></script>
+
+        <!-- Timepicker js -->
+        <script src="<?php echo base_url('public/assets/plugins/time-picker/jquery.timepicker.js'); ?>"></script>
+        <script src="<?php echo base_url('public/assets/plugins/time-picker/toggles.min.js'); ?>"></script>
+
+        <!-- Datepicker js -->
+        <script src="<?php echo base_url('public/assets/plugins/date-picker/date-picker.js'); ?>"></script>
+        <script src="<?php echo base_url('public/assets/plugins/date-picker/jquery-ui.js'); ?>"></script>
+        <script src="<?php echo base_url('public/assets/plugins/input-mask/jquery.maskedinput.js'); ?>"></script>
+
+        <!--File-Uploads Js-->
+        <script src="<?php echo base_url('public/assets/plugins/fancyuploder/jquery.ui.widget.js'); ?>"></script>
+        <script src="<?php echo base_url('public/assets/plugins/fancyuploder/jquery.fileupload.js'); ?>"></script>
+        <script src="<?php echo base_url('public/assets/plugins/fancyuploder/jquery.iframe-transport.js'); ?>"></script>
+        <script src="<?php echo base_url('public/assets/plugins/fancyuploder/jquery.fancy-fileupload.js'); ?>"></script>
+        <script src="<?php echo base_url('public/assets/plugins/fancyuploder/fancy-uploader.js'); ?>"></script>
+
+        <!-- File uploads js -->
+        <script src="<?php echo base_url('public/assets/plugins/fileupload/js/dropify.js'); ?>"></script>
+        <script src="<?php echo base_url('public/assets/js/filupload.js'); ?>"></script>
+
+        <!-- Multiple select js -->
+        <script src="<?php echo base_url('public/assets/plugins/multipleselect/multiple-select.js'); ?>"></script>
+        <script src="<?php echo base_url('public/assets/plugins/multipleselect/multi-select.js'); ?>"></script>
+
+        <!--Sumoselect js-->
+        <script src="<?php echo base_url('public/assets/plugins/sumoselect/jquery.sumoselect.js'); ?>"></script>
+
+        <!--intlTelInput js-->
+        <script src="<?php echo base_url('public/assets/plugins/intl-tel-input-master/intlTelInput.js'); ?>"></script>
+        <script src="<?php echo base_url('public/assets/plugins/intl-tel-input-master/country-select.js'); ?>"></script>
+        <script src="<?php echo base_url('public/assets/plugins/intl-tel-input-master/utils.js'); ?>"></script>
+
+        <!--jquery transfer js-->
+        <script src="<?php echo base_url('public/assets/plugins/jQuerytransfer/jquery.transfer.js'); ?>"></script>
+
+        <!--multi js-->
+        <script src="<?php echo base_url('public/assets/plugins/multi/multi.min.js'); ?>"></script>
+
+        <!-- Form Advanced Element -->
+        <script src="<?php echo base_url('public/assets/js/formelementadvnced.js'); ?>"></script>
+        <script src="<?php echo base_url('public/assets/js/form-elements.js'); ?>"></script>
+        <script src="<?php echo base_url('public/assets/js/file-upload.js'); ?>"></script>
+
+        <!-- popover js -->
+        <script src="<?php echo base_url('public/assets/js/popover.js'); ?>"></script>
+
+        <!-- Data tables js-->
+        <script src="<?php echo base_url('public/assets/plugins/datatable/js/jquery.dataTables.js') ?>"></script>
+        <script src="<?php echo base_url('public/assets/plugins/datatable/js/dataTables.bootstrap4.js') ?>"></script>
+        <script src="<?php echo base_url('public/assets/plugins/datatable/js/dataTables.buttons.min.js') ?>"></script>
+        <script src="<?php echo base_url('public/assets/plugins/datatable/js/buttons.bootstrap4.min.js') ?>"></script>
+        <script src="<?php echo base_url('public/assets/plugins/datatable/js/jszip.min.js') ?>"></script>
+        <script src="<?php echo base_url('public/assets/plugins/datatable/js/pdfmake.min.js') ?>"></script>
+        <script src="<?php echo base_url('public/assets/plugins/datatable/js/vfs_fonts.js') ?>"></script>
+        <script src="<?php echo base_url('public/assets/plugins/datatable/js/buttons.html5.min.js') ?>"></script>
+        <script src="<?php echo base_url('public/assets/plugins/datatable/js/buttons.print.min.js') ?>"></script>
+        <script src="<?php echo base_url('public/assets/plugins/datatable/js/buttons.colVis.min.js') ?>"></script>
+        <script src="<?php echo base_url('public/assets/plugins/datatable/dataTables.responsive.min.js') ?>"></script>
+        <script src="<?php echo base_url('public/assets/plugins/datatable/responsive.bootstrap4.min.js') ?>"></script>
+        <script src="<?php echo base_url('public/assets/js/datatables.js') ?>"></script>
+
+        <!-- Notifications js -->
+        <script src="<?php echo base_url('public/assets/plugins/notify/js/rainbow.js'); ?>"></script>
+        <script src="<?php echo base_url('public/assets/plugins/notify/js/sample.js'); ?>"></script>
+        <script src="<?php echo base_url('public/assets/plugins/notify/js/jquery.growl.js'); ?>"></script>
+        <script src="<?php echo base_url('public/assets/plugins/notify/js/notifIt.js'); ?>"></script>
+
+    <!-- INTERNAL JS INDEX END -->
+
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $('#editable-sample').DataTable({});
+        })
+    </script>
+
+    </body>
+</html>
