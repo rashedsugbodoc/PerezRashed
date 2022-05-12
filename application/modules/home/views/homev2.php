bddeb5397ffc08033b692c98e2f217c2e2f8a90d
kenneth <kenneth.rygeltech@gmail.com>
2022-05-12 17:50:41 +0800

    Update Admin Dashboard by Fixing Tablet Device Responsiveness for Income,Expense,Appointment Cards

--- application/modules/home/views/homev2.php
+++ application/modules/home/views/homev2.php
@@ -294,7 +294,7 @@
                                             <?php } ?>
                                         <?php } ?>
 
-                                        <div class="col-md-4 col-sm-12">
+                                        <div class="col-md-12 col-sm-12 col-lg-4">
                                             <div class="card">
                                                 <div class="card-header">
                                                     <h3 class="card-title"><?php echo date('D, d F Y'); ?></h3>
@@ -335,7 +335,7 @@
                                             </div>
                                         </div>
 
-                                        <div class="col-md-4 col-sm-12">
+                                        <div class="col-md-12 col-sm-12 col-lg-4">
                                             <div class="card">
                                                 <div class="card-header">
                                                     <h3 class="card-title"><?php echo date('F Y'); ?></h3>
@@ -376,7 +376,7 @@
                                             </div>
                                         </div>
 
-                                        <div class="col-md-4 col-sm-12">
+                                        <div class="col-md-12 col-sm-12 col-lg-4">
                                             <div class="card">
                                                 <div class="card-header">
                                                     <h3 class="card-title"><?php echo date('Y'); ?></h3>
