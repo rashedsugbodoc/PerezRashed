6863b351f3047d6f18670efc8e8b57d09786bb56
kenneth <kenneth.rygeltech@gmail.com>
2022-05-31 19:17:13 +0800

    Fix Ryan Bug 35 - Change Medical History Patient Profile ID Value from Patient->id to Patient->patient_id

--- application/modules/patient/views/medical_historyv2.php
+++ application/modules/patient/views/medical_historyv2.php
@@ -272,7 +272,7 @@
                                                 <div class="col-md-12">
                                                     <div class="d-flex mb-1">
                                                         <svg class="svg-icon" xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><g><rect fill="none" height="24" width="24"/></g><g><path d="M20,10V8h-4V4h-2v4h-4V4H8v4H4v2h4v4H4v2h4v4h2v-4h4v4h2v-4h4v-2h-4v-4H20z M14,14h-4v-4h4V14z"/></g></svg>
-                                                        <div class="h6 mb-0 ml-1 mt-1"><?php echo $patient->id; ?></div>
+                                                        <div class="h6 mb-0 ml-1 mt-1"><?php echo $patient->patient_id; ?></div>
                                                     </div>
                                                 </div>
                                             </div>
