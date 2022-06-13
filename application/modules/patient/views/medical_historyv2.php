f0a54cb8586812f301953805ac955f1405f3e83a
kenneth <kenneth.rygeltech@gmail.com>
2022-06-13 16:55:09 +0800

    Update Medical History by changing Encounter Add Button Link Get Request From patient->id to patient->patient_id

--- application/modules/patient/views/medical_historyv2.php
+++ application/modules/patient/views/medical_historyv2.php
@@ -1161,7 +1161,7 @@
                                                             <?php if ($this->ion_auth->in_group(array('Doctor'))) { ?>
                                                                 <div class=" no-print">
                                                                     <?php if(!empty($encounter_id)) { ?>
-                                                                        <a class="btn btn-primary btn_width btn-xs" href="encounter/addNewView?patient_id=<?php echo $patient->id.'&root=patient&method=medicalHistory' ?>">
+                                                                        <a class="btn btn-primary btn_width btn-xs" href="encounter/addNewView?patient_id=<?php echo $patient->patient_id.'&root=patient&method=medicalHistory' ?>">
                                                                             <i class="fa fa-plus"> </i> <?php echo lang('add_new'); ?> 
                                                                         </a>
                                                                     <?php } ?>
