22ebaae2069b4f72a4e33070afcf77026155907e
Ryan Michael Gelig <ryan.gelig@gmail.com>
2022-06-18 01:30:05 +0800

    Modify Ion_auth forgotten_password method by removing requirement for user to have active=1 value prior to sending reset password email

--- application/libraries/Ion_auth.php
+++ application/libraries/Ion_auth.php
@@ -134,7 +134,7 @@ class Ion_auth {
         if ($this->ion_auth_model->forgotten_password($identity)) {   //changed
             // Get user information
             $identifier = $this->ion_auth_model->identity_column; // use model identity column, so it can be overridden in a controller
-            $user = $this->where($identifier, $identity)->where('active', 1)->users()->row();  //changed to get_user_by_identity from email
+            $user = $this->where($identifier, $identity)->users()->row();  //changed to get_user_by_identity from email
 
             if ($user) {
                 $data = array(
