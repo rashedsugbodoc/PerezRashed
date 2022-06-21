624fef227ec1224618ec2b3f0f33d4a7b1cdecec
kenneth <kenneth.rygeltech@gmail.com>
2022-06-21 16:41:15 +0800

    Update Appointment By Doctorv2 by changing Export buttons to Export Dropdown

--- application/modules/appointment/views/appointment_by_doctorv2.php
+++ application/modules/appointment/views/appointment_by_doctorv2.php
@@ -320,16 +320,49 @@
                         "<'row'<'col-sm-12'tr>>" +
                         "<'row'<'col-sm-5'i><'col-sm-7'p>>",
                 buttons: [
-                    'copyHtml5',
-                    'excelHtml5',
-                    'csvHtml5',
-                    'pdfHtml5',
                     {
-                        extend: 'print',
-                        exportOptions: {
-                            columns: [0, 1, 2, 3],
-                        }
-                    },
+                        extend: 'collection',
+                        text: 'Export <i class="fe fe-chevron-down"><i>',
+                        buttons: [
+                            {
+                                extend: 'copyHtml5',
+                                title: '<?php echo lang('appointment') . ' ' . lang('list');?>',
+                                exportOptions: {
+                                    columns: [1, 2, 3],
+                                }
+                            },
+                            {
+                                extend: 'excelHtml5',
+                                title: '<?php echo lang('appointment') . ' ' . lang('list');?>',
+                                exportOptions: {
+                                    columns: [1, 2, 3],
+                                }
+                            },
+                            {
+                                extend: 'csvHtml5',
+                                title: '<?php echo lang('appointment') . ' ' . lang('list');?>',
+                                exportOptions: {
+                                    columns: [1, 2, 3],
+                                }
+                            },
+                            {
+                                extend: 'pdfHtml5',
+                                title: '<?php echo lang('appointment') . ' ' . lang('list');?>',
+                                exportOptions: {
+                                    columns: [1, 2, 3],
+                                },
+                                orientation: 'portrait',
+                                pageSize: 'LEGAL'
+                            },
+                            {
+                                extend: 'print',
+                                title: '<?php echo lang('appointment') . ' ' . lang('list');?>',
+                                exportOptions: {
+                                    columns: [1, 2, 3],
+                                }
+                            }
+                        ]
+                    }
                 ],
                 aLengthMenu: [
                     [10, 25, 50, 100, -1],
