45988883c67804bbb0b697b648c44e7ccca66c73
Ryan Michael Gelig <ryan.gelig@gmail.com>
2022-08-09 10:38:48 +0800

    Modify dashboardv2 by fixing side menu links and clickability for Payer Accounts, Settings, Email, Create New Hospital, All Hospitals, Packages, Add New Package, Requests

--- application/modules/home/views/dashboardv2.php
+++ application/modules/home/views/dashboardv2.php
@@ -945,27 +945,35 @@ if (!$this->ion_auth->in_group(array('superadmin'))) {
 
                         <?php if ($this->ion_auth->in_group('superadmin')) { ?>
                             <li class="slide">
-                                <a class="side-menu__item"  data-toggle="slide" href="hospital">
+                                <a class="side-menu__item"  data-toggle="slide" href="javascript:;">
+                                <svg class="side-menu__icon" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><circle cx="12" cy="8" opacity="1" r="2.1"/><path d="M12 14.9c-2.97 0-6.1 1.46-6.1 2.1v1.1h12.2V17c0-.64-3.13-2.1-6.1-2.1z" opacity="1"/><path fill="currentColor" d="M12 13c-2.67 0-8 1.34-8 4v3h16v-3c0-2.66-5.33-4-8-4zm6.1 5.1H5.9V17c0-.64 3.13-2.1 6.1-2.1s6.1 1.46 6.1 2.1v1.1zM12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0-6.1c1.16 0 2.1.94 2.1 2.1 0 1.16-.94 2.1-2.1 2.1S9.9 9.16 9.9 8c0-1.16.94-2.1 2.1-2.1z"/></svg>
+                                <span class="side-menu__label"><?php echo lang('accounts'); ?></span><i class="angle fa fa-angle-right"></i></a>
+                                <ul class="slide-menu">
+                                    <li><a href="company" class="slide-item"><?php echo lang('payer_accounts'); ?></a></li>
+                                </ul>
+                            </li>
+                            <li class="slide">
+                                <a class="side-menu__item"  href="hospital">
                                 <svg class="side-menu__icon" xmlns="http://www.w3.org/2000/svg" width="24" height="26" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>
                                 <span class="side-menu__label"><?php echo lang('all_hospitals'); ?></span></a>
                             </li>
                             <li class="slide">
-                                <a class="side-menu__item"  data-toggle="slide" href="hospital/addNewView">
+                                <a class="side-menu__item"  href="hospital/addNewView">
                                 <svg class="side-menu__icon" xmlns="http://www.w3.org/2000/svg" width="24" height="26" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>
                                 <span class="side-menu__label"><?php echo lang('create_new_hospital'); ?></span></a>
                             </li>
                             <li class="slide">
-                                <a class="side-menu__item"  data-toggle="slide" href="hospital/package">
+                                <a class="side-menu__item"  href="hospital/package">
                                 <svg class="side-menu__icon" xmlns="http://www.w3.org/2000/svg" width="24" height="26" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>
                                 <span class="side-menu__label"><?php echo lang('packages'); ?></span></a>
                             </li>
                             <li class="slide">
-                                <a class="side-menu__item"  data-toggle="slide" href="hospital/package/addNewView">
+                                <a class="side-menu__item"  href="hospital/package/addNewView">
                                 <svg class="side-menu__icon" xmlns="http://www.w3.org/2000/svg" width="24" height="26" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>
                                 <span class="side-menu__label"><?php echo lang('add_new_package'); ?></span></a>
                             </li>
                             <li class="slide">
-                                <a class="side-menu__item"  data-toggle="slide" href="request">
+                                <a class="side-menu__item"  href="request">
                                 <svg class="side-menu__icon" xmlns="http://www.w3.org/2000/svg" width="24" height="26" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>
                                 <span class="side-menu__label"><?php echo lang('requests'); ?></span></a>
                             </li>
@@ -979,8 +987,44 @@ if (!$this->ion_auth->in_group(array('superadmin'))) {
                                     <li><a href="slide" class="slide-item"><?php echo lang('new'); ?></a></li>
                                     <li><a href="service" class="slide-item"><?php echo lang('services'); ?></a></li>
                                 </ul>
-                            </li>         
-                        <?php  } ?>                                                                  
+                            </li>
+                            <li class="slide">
+                                <a class="side-menu__item" data-toggle="slide" href="javascript:;">
+                                <svg class="side-menu__icon" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M19.28 8.6l-.7-1.21-1.27.51-1.06.43-.91-.7c-.39-.3-.8-.54-1.23-.71l-1.06-.43-.16-1.13L12.7 4h-1.4l-.19 1.35-.16 1.13-1.06.44c-.41.17-.82.41-1.25.73l-.9.68-1.05-.42-1.27-.52-.7 1.21 1.08.84.89.7-.14 1.13c-.03.3-.05.53-.05.73s.02.43.05.73l.14 1.13-.89.7-1.08.84.7 1.21 1.27-.51 1.06-.43.91.7c.39.3.8.54 1.23.71l1.06.43.16 1.13.19 1.36h1.39l.19-1.35.16-1.13 1.06-.43c.41-.17.82-.41 1.25-.73l.9-.68 1.04.42 1.27.51.7-1.21-1.08-.84-.89-.7.14-1.13c.04-.31.05-.52.05-.73 0-.21-.02-.43-.05-.73l-.14-1.13.89-.7 1.1-.84zM12 16c-2.21 0-4-1.79-4-4s1.79-4 4-4 4 1.79 4 4-1.79 4-4 4z" opacity="1"/><path fill="currentColor" d="M19.43 12.98c.04-.32.07-.64.07-.98 0-.34-.03-.66-.07-.98l2.11-1.65c.19-.15.24-.42.12-.64l-2-3.46c-.09-.16-.26-.25-.44-.25-.06 0-.12.01-.17.03l-2.49 1c-.52-.4-1.08-.73-1.69-.98l-.38-2.65C14.46 2.18 14.25 2 14 2h-4c-.25 0-.46.18-.49.42l-.38 2.65c-.61.25-1.17.59-1.69.98l-2.49-1c-.06-.02-.12-.03-.18-.03-.17 0-.34.09-.43.25l-2 3.46c-.13.22-.07.49.12.64l2.11 1.65c-.04.32-.07.65-.07.98s.03.66.07.98l-2.11 1.65c-.19.15-.24.42-.12.64l2 3.46c.09.16.26.25.44.25.06 0 .12-.01.17-.03l2.49-1c.52.4 1.08.73 1.69.98l.38 2.65c.03.24.24.42.49.42h4c.25 0 .46-.18.49-.42l.38-2.65c.61-.25 1.17-.59 1.69-.98l2.49 1c.06.02.12.03.18.03.17 0 .34-.09.43-.25l2-3.46c.12-.22.07-.49-.12-.64l-2.11-1.65zm-1.98-1.71c.04.31.05.52.05.73 0 .21-.02.43-.05.73l-.14 1.13.89.7 1.08.84-.7 1.21-1.27-.51-1.04-.42-.9.68c-.43.32-.84.56-1.25.73l-1.06.43-.16 1.13-.2 1.35h-1.4l-.19-1.35-.16-1.13-1.06-.43c-.43-.18-.83-.41-1.23-.71l-.91-.7-1.06.43-1.27.51-.7-1.21 1.08-.84.89-.7-.14-1.13c-.03-.31-.05-.54-.05-.74s.02-.43.05-.73l.14-1.13-.89-.7-1.08-.84.7-1.21 1.27.51 1.04.42.9-.68c.43-.32.84-.56 1.25-.73l1.06-.43.16-1.13.2-1.35h1.39l.19 1.35.16 1.13 1.06.43c.43.18.83.41 1.23.71l.91.7 1.06-.43 1.27-.51.7 1.21-1.07.85-.89.7.14 1.13zM12 8c-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4zm0 6c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2z"/></svg>
+                                <span class="side-menu__label"><?php echo lang('settings'); ?></span><i class="angle fa fa-angle-right"></i></a>
+                                <ul class="slide-menu">
+                                    <li><a href="settings" class="slide-item"><?php echo lang('system_settings'); ?></a></li>
+                                    <li><a href="pgateway" class="slide-item"><?php echo lang('payment_gateway'); ?></a></li>
+                                    <li><a href="settings/language" class="slide-item"><?php echo lang('language'); ?></a></li>
+                                </ul>
+                            </li>
+                            <li class="slide">
+                                <a class="side-menu__item" data-toggle="slide" href="javascript:;">
+                                <svg class="side-menu__icon" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M20 8l-8 5-8-5v10h16zm0-2H4l8 4.99z" opacity="1"/><path fill="currentColor" d="M4 20h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2zM20 6l-8 4.99L4 6h16zM4 8l8 5 8-5v10H4V8z"/></svg>
+                                <span class="side-menu__label"><?php echo lang('email'); ?></span><i class="angle fa fa-angle-right"></i></a>
+                                <ul class="slide-menu">
+                                    <li><a href="email/autoEmailTemplate" class="slide-item"><?php echo lang('autoemailtemplate'); ?></a></li>
+                                    <li><a href="email/sendView" class="slide-item"><?php echo lang('new'); ?></a></li>
+                                    <li><a href="email/sent" class="slide-item"><?php echo lang('sent'); ?></a></li>
+                                    <?php if ($this->ion_auth->in_group(array('admin'))) { ?>                                        
+                                        <li><a href="email/settings" class="slide-item"><?php echo lang('settings'); ?></a></li>
+                                    <?php } ?>
+                                </ul>
+                            </li>
+                            <?php if (in_array('sms', $this->super_modules)) { ?>
+                            <li class="slide">
+                                <a class="side-menu__item" data-toggle="slide" href="javascript:;">
+                                <svg class="side-menu__icon" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M4 18l2-2h14V4H4v14zm11-9h2v2h-2V9zm-4 0h2v2h-2V9zM7 9h2v2H7V9z" opacity="1"/><path fill="currentColor" d="M20 2H4c-1.1 0-2 .9-2 2v18l4-4h14c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm0 14H6l-2 2V4h16v12zM7 9h2v2H7zm4 0h2v2h-2zm4 0h2v2h-2z"/></svg>
+                                <span class="side-menu__label"><?php echo lang('sms'); ?></span><i class="angle fa fa-angle-right"></i></a>
+                                <ul class="slide-menu">
+                                    <li><a href="sms/autoSMSTemplate" class="slide-item"><?php echo lang('autosmstemplate'); ?></a></li>
+                                    <li><a href="sms/sendView" class="slide-item"><?php echo lang('write_message'); ?></a></li>
+                                    <li><a href="sms/sent" class="slide-item"><?php echo lang('sent_messages'); ?></a></li>
+                                    <li><a href="sms/settings" class="slide-item"><?php echo lang('sms_settings'); ?></a></li>
+                                </ul>
+                            </li>
+                            <?php } ?>
+                        <?php  } ?>
 
                         <!-- <li class="slide">
                             <a class="side-menu__item" href="profile">
