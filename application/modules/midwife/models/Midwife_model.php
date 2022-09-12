c7aa106479869d747eeb27a226e7eba010305b03
kenneth <kenneth.rygeltech@gmail.com>
2022-09-12 17:59:24 +0800

    SUGBOWEB1-80 Add Midwife Module it includes the Modification(Add, Update, Delete, View) of Data

---
+++ application/modules/midwife/models/Midwife_model.php
@@ -0,0 +1,60 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+class Midwife_model extends CI_model {
+
+    function __construct() {
+        parent::__construct();
+        $this->load->database();
+    }
+
+    function insertMidwife($data) {
+        $data1 = array('hospital_id' => $this->session->userdata('hospital_id'));
+        $data2 = array_merge($data, $data1);
+        $this->db->insert('midwife', $data2);
+    }
+
+    function getMidwife() {
+        $this->db->where('hospital_id', $this->session->userdata('hospital_id'));
+        $this->db->order_by('id', 'desc');
+        $query = $this->db->get('midwife');
+        return $query->result();
+    }
+
+    function getMidwifeById($id) {
+        $this->db->where('hospital_id', $this->session->userdata('hospital_id'));
+        $this->db->where('id', $id);
+        $query = $this->db->get('midwife');
+        return $query->row();
+    }
+
+    function updateMidwife($id, $data) {
+        $this->db->where('id', $id);
+        $this->db->update('midwife', $data);
+    }
+
+    function delete($id) {
+        $this->db->where('id', $id);
+        $this->db->delete('midwife');
+    }
+
+    function updateIonUser($username, $email, $password, $ion_user_id) {
+        $uptade_ion_user = array(
+            'username' => $username,
+            'email' => $email,
+            'password' => $password
+        );
+        $this->db->where('id', $ion_user_id);
+        $this->db->update('users', $uptade_ion_user);
+    }
+
+    function getMidwifeByIonUserId($id) {
+        $this->db->where('hospital_id', $this->session->userdata('hospital_id'));
+        $this->db->where('ion_user_id', $id);
+        $query = $this->db->get('midwife');
+        return $query->row();
+    }
+
+}
