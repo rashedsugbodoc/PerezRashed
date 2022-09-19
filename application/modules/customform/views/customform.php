3d4f59dce0d8bf316365f1010009d4c3ac497a0b
kenneth <kenneth.rygeltech@gmail.com>
2022-09-19 14:27:13 +0800

    SUGBOWEB1-97 include Doctor Id inside the Count Model Method to display list of CUstom Form that the Logged in Doctor Created

--- application/modules/customform/views/customform.php
+++ application/modules/customform/views/customform.php
@@ -22,9 +22,15 @@
                             <div class="col-lg-4 col-xl-3 col-md-12 col-sm-12">
                                 <div class="card">
                                     <div class="list-group list-group-transparent mb-0 mail-inbox pb-3">
+                                        <?php
+                                            $user = $this->ion_auth->get_user_id();
+                                            if ($this->ion_auth->in_group(array('Doctor'))) {
+                                                $doctor_id = $this->doctor_model->getDoctorByIonUserId($user)->id;
+                                            }
+                                        ?>
                                         <?php foreach($customform_types as $cft) { ?>
                                             <a id="<?php echo $cft->name ?>" class="list-group-item list-group-item-action d-flex align-items-center customform_type" data-id="<?php echo $cft->id; ?>" data-method="<?php echo $cft->method_name; ?>" data-name="<?php echo $cft->name; ?>" data-displayname="<?php echo $cft->display_name; ?>">
-                                                <svg class="svg-icon mr-2" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M20 8l-8 5-8-5v10h16zm0-2H4l8 4.99z" opacity=".3"/><path d="M4 20h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2zM20 6l-8 4.99L4 6h16zM4 8l8 5 8-5v10H4V8z"/></svg> <?php echo $cft->display_name; ?> <span class="ml-auto badge badge-success"><?php echo $this->customform_model->getCustomByTypeCount($cft->id); ?></span>
+                                                <svg class="svg-icon mr-2" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M20 8l-8 5-8-5v10h16zm0-2H4l8 4.99z" opacity=".3"/><path d="M4 20h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2zM20 6l-8 4.99L4 6h16zM4 8l8 5 8-5v10H4V8z"/></svg> <?php echo $cft->display_name; ?> <span class="ml-auto badge badge-success"><?php echo $this->customform_model->getCustomByTypeCount($cft->id, $doctor_id); ?></span>
                                             </a>
                                         <?php } ?>
                                     </div>
