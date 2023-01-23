28158fbd567a08fd98388626d13db9a66a4f47b4
kenneth <kenneth.rygeltech@gmail.com>
2023-01-23 11:29:30 +0800

    SUGBOWEB1-256 Fix Finance Monthly Undefined Variable Error
    *  Update Finance Controller monthly Method by adding Data Variable
       Months(Jan, Feb, Mar, Apr, May, June, Jul, Aug, Sep, Oct, Nov, Dec)
       with a value of null and add Data Variable Settings.
    *  Update Monthlyv2 by adding shorthand if Statement to Month and
       Settings Variables.

--- application/modules/finance/controllers/Finance.php
+++ application/modules/finance/controllers/Finance.php
@@ -4618,10 +4618,23 @@ class Finance extends MX_Controller {
             }
         }
 
+        $data['settings'] = $this->settings_model->getSettings();
         $data['year'] = $year;
         $data['first_minute'] = $first_minute;
         $data['last_minute'] = $last_minute;
         $data['all_payments'] = $all_payments;
+        $data['jan_total'] = null;
+        $data['feb_total'] = null;
+        $data['mar_total'] = null;
+        $data['apr_total'] = null;
+        $data['may_total'] = null;
+        $data['jun_total'] = null;
+        $data['jul_total'] = null;
+        $data['aug_total'] = null;
+        $data['sep_total'] = null;
+        $data['oct_total'] = null;
+        $data['nov_total'] = null;
+        $data['dec_total'] = null;
 
         $this->load->view('home/dashboardv2', $data); // just the header file
         $this->load->view('monthlyv2', $data);
