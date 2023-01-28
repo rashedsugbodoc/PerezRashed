6f4d0a3375dfc1675e9614a6dac48915bd47a49b
kenneth <kenneth.rygeltech@gmail.com>
2023-01-28 11:04:22 +0800

    SUGBOWEB1-256 Revert Labrequest Model getLabrequestByPatientId Method
    *  Update Labrequest Model by Reverting the Method getLabrequestByPatientId to the original structure .

--- application/modules/labrequest/models/Labrequest_model.php
+++ application/modules/labrequest/models/Labrequest_model.php
@@ -140,7 +140,7 @@ class Labrequest_model extends CI_model {
         $this->db->order_by('id', 'desc');
         $this->db->group_by('lab_request_number');
         $query = $this->db->get('lab_request');
-        return $query->row();
+        return $query->result();
     }
 
     function getLabrequestByPatientIdByEncounterId($id, $encounter_id) {
